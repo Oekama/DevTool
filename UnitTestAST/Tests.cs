7bd5fc188c15bf0a26f45fbde3d9079c05d6c42e
Inardomon <Inardomon@gmail.com>
2017-04-24 21:53:05 +0300

    More tests. Add multiplication node.

--- UnitTestAST/Tests.cs
+++ UnitTestAST/Tests.cs
@@ -31,6 +31,88 @@ namespace UnitTestAST
             var addAction = (Func<int>) add.CreateDelegate(typeof(Func<int>));
 
             Assert.AreEqual(4, addAction());
+
         }
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            var mulCode = new MultiplicationNode(new IntConstNode(2), new IntConstNode(2));
+
+            Type[] methodArgs = {};
+
+            var mul = new DynamicMethod(
+                "Multiplication",
+                typeof(int),
+                methodArgs,
+                typeof(NodesTests).Module);
+
+            var il = mul.GetILGenerator();
+
+            foreach (var operation in mulCode.GetOperationsList())
+                operation.Emit(il);
+
+            il.Emit(OpCodes.Ret);
+
+            var mulAction = (Func<int>) mul.CreateDelegate(typeof(Func<int>));
+
+            Assert.AreEqual(4, mulAction());
+
+        }
+
+        [Test]
+        public void AddMultiplicationTest()
+        {
+            var code = new MultiplicationNode(new AddNode(new IntConstNode(2),new IntConstNode(2) ), new IntConstNode(2));
+
+            Type[] methodArgs = {};
+
+            var met = new DynamicMethod(
+                "Multiplication",
+                typeof(int),
+                methodArgs,
+                typeof(NodesTests).Module);
+
+            var il = met.GetILGenerator();
+
+            foreach (var operation in code.GetOperationsList())
+                operation.Emit(il);
+
+            il.Emit(OpCodes.Ret);
+
+            var act = (Func<int>) met.CreateDelegate(typeof(Func<int>));
+
+            Assert.AreEqual(8, act());
+
+        }
+
+        [Test]
+        public void MultiplicationAddTest()
+        {
+            var code = new AddNode(new MultiplicationNode(new IntConstNode(2),new IntConstNode(2) ), new IntConstNode(2));
+
+            Type[] methodArgs = {};
+
+            var met = new DynamicMethod(
+                "Multiplication",
+                typeof(int),
+                methodArgs,
+                typeof(NodesTests).Module);
+
+            var il = met.GetILGenerator();
+
+            foreach (var operation in code.GetOperationsList())
+                operation.Emit(il);
+
+            il.Emit(OpCodes.Ret);
+
+            var act = (Func<int>) met.CreateDelegate(typeof(Func<int>));
+
+            Assert.AreEqual(6, act());
+
+        }
+
+
+
     }
 }
